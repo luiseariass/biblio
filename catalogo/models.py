db121a1e09bdec9d1c98f1d33da79a1140dfe8ff
luiseariass <luisearias94@gmail.com>
2020-05-27 18:23:16 -0500

    Probando el boton de descarga

--- catalogo/models.py
+++ catalogo/models.py
@@ -50,7 +50,7 @@ class Book(models.Model):
     def file_link(self):
 
         if self.file:
-            return ("{}".format(self.file.url) + chr(37)+"27")
+            return ("{}".format(self.file.url))
         else:
             return "No attachment"
 
